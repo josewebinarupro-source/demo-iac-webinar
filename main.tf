43e6da7b1b8dcaa096e9e3d8385983d64511779e
josewebinarupro <jose.webinar.upro@gmail.com>
2026-06-07 11:35:43 +0200

    Demo IaC Webinar - Terraform + Docker

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web" {
+  name  = "webinar-web"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
+  volumes {
+    host_path      = "/home/joseac/demo-webinar-upro/demo-iac/html"
+    container_path = "/usr/share/nginx/html"
+  }
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:15"
+}
+
+resource "docker_container" "db" {
+  name  = "webinar-db"
+  image = docker_image.postgres.image_id
+  env = [
+    "POSTGRES_PASSWORD=webinar2026",
+    "POSTGRES_DB=demodb"
+  ]
+  ports {
+    internal = 5432
+    external = 5432
+  }
+}
+
+resource "docker_image" "pgadmin" {
+  name = "dpage/pgadmin4:latest"
+}
+
+resource "docker_container" "pgadmin" {
+  name  = "webinar-pgadmin"
+  image = docker_image.pgadmin.image_id
+  env = [
+    "PGADMIN_DEFAULT_EMAIL=admin@webinar.com",
+    "PGADMIN_DEFAULT_PASSWORD=webinar2026"
+  ]
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
+resource "docker_image" "portainer" {
+  name = "portainer/portainer-ce:latest"
+}
+
+resource "docker_volume" "portainer_data" {
+  name = "portainer_data"
+}
+
+resource "docker_container" "portainer" {
+  name  = "webinar-portainer"
+  image = docker_image.portainer.image_id
+  ports {
+    internal = 9000
+    external = 9000
+  }
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+  volumes {
+    volume_name    = docker_volume.portainer_data.name
+    container_path = "/data"
+  }
+}

cc9527e23b028a890e62fdcf9dc3eccb273cf463
josewebinarupro <jose.webinar.upro@gmail.com>
2026-06-07 18:03:58 +0200

    Añadida variable app_port

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "base_path" {
   description = "Ruta base del proyecto"
   default     = "/home/joseac/demo-iac-webinar"
 }
+
+variable "app_port" {
+  description = "Puerto externo de la app"
+  default     = 5000
+}
